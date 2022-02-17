dee18f7a38ec98136cc503210f6df57d312285a3
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-02-16 21:28:41 -0500

    updated deployment to include hello world contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const HelloWorld = artifacts.require("HelloWorld");
+const SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function(deployer) {
+  deployer.deploy(HelloWorld);
   deployer.deploy(SimpleStorage);
 };
