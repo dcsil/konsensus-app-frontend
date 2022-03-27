fb26cf4817d31d3b956bba82e684de574e56aef7
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-03-27 17:32:12 -0400

    updated test settings

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
 
-import { shallow, configure } from 'enzyme';
+import { configure } from 'enzyme';
 
 configure({ adapter: new Adapter() });
-
-it('renders without crashing', () => {
-  shallow(<App />);
-});
 
 it('app mounts properly', () => {
   const div = document.createElement('div');
