8fee26965823a4fd2aa6fe12ab2f3675a652407b
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-03-31 15:33:58 -0400

    updated file card with icon, name etc.

--- client/src/components/File/FileCard.js
+++ client/src/components/File/FileCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FileIcon, defaultStyles } from 'react-file-icon';
+
 // reactstrap components
 import {
   Button,
@@ -8,36 +10,80 @@ import {
   CardTitle,
   Row,
   Col,
+  Container,
 } from 'reactstrap';
 
-const FileCard = () => {
+import { useEffect, useState, useContext } from 'react';
+import { GlobalStateContext } from 'components/GlobalState';
+import { getFileById } from 'components/Auth/authFunctions';
+import Moment from 'react-moment';
+import FilePreviewerThumbnail from 'react-file-previewer';
+
+const FileCard = ({ fileId }) => {
+  const [file, setFile] = useState(null);
+
+  console.log(fileId);
+
+  const globalState = useContext(GlobalStateContext);
+  useEffect(() => {
+    getFileById(globalState.token, fileId, setFile);
+  }, []);
+
+  const fileTypeHelper = (fileName) => {
+    const split = fileName.split('.');
+    return split[1];
+  };
+
   return (
     <>
-      <Card style={{ width: '18rem', height: '8rem' }}>
-        <CardImg
-          alt="..."
-          style={{ maxHeight: '6rem', objectFit: 'cover' }}
-          src={require('assets/img/theme/chibi_julian.png').default}
-          top
-        />
-        <CardBody>
-          <Row>
-            <div className="col">
-              <CardTitle className="text-uppercase text-muted mb-0">
-                Total traffic
-              </CardTitle>
-              <span className="h2 font-weight-bold mb-0">
-                350,897
-              </span>
-            </div>
-            <Col className="col-auto">
-              <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                <i className="fas fa-chart-bar" />
-              </div>
-            </Col>
-          </Row>
-        </CardBody>
-      </Card>
+      {file !== null && (
+        <Card
+          style={{
+            width: '18rem',
+            height: '8rem',
+            cursor: 'pointer',
+          }}
+          // onClick={() => {
+          //   window.open(file.url);
+          // }}
+        >
+          <CardBody>
+            {/* <FilePreviewerThumbnail
+              style={{ objectFit: 'cover' }}
+              hideControls={true}
+              file={{
+                url: file.url,
+              }}
+            /> */}
+            <Row>
+              <Col lg={{ size: 'auto' }}>
+                <div style={{ width: '2.5rem' }}>
+                  <FileIcon
+                    extension={fileTypeHelper(file.name)}
+                    {...defaultStyles[fileTypeHelper(file.name)]}
+                  />
+                </div>
+              </Col>
+              <Col>
+                <CardTitle className="font-weight-bold mb-0">
+                  {file.name}
+                </CardTitle>
+              </Col>
+            </Row>
+            <Row className="pt-2">
+              <Col>
+                <span className="h6 text-muted mb-0">
+                  Last updated on{' '}
+                  <Moment
+                    format="MMM DD/YYYY hh:mm"
+                    date={file.updatedAt}
+                  />
+                </span>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      )}
     </>
   );
 };
