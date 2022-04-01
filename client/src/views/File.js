dd6226f40848cb2e6a464ea7ceb6db44e4f439fa
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-03-31 23:21:11 -0400

    file page

--- client/src/views/File.js
+++ client/src/views/File.js
@@ -1,35 +1,33 @@
 import { getFiles } from 'components/Auth/authFunctions';
 import React from 'react';
-import FilePreviewer from 'react-file-previewer';
+import axios from 'axios';
 
 // reactstrap components
 import { Container, Row, Col } from 'reactstrap';
 
+// core components
+import { useState } from 'react';
+
 import Header from 'components/Headers/Header.js';
-import FileCard from 'components/File/FileCard';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import ImageViewer from 'components/File/ImageViewer';
 
 const File = () => {
   let history = useHistory();
-  const { fileId, url } = history.location.state;
-  console.log(fileId);
-  // const [ownedFiles, setOwnedFiles] = useState([]);
-  // const [recentFiles, setRecentFiles] = useState([]);
-  // const [starredFiles, setStarredFiles] = useState([]);
-
-  // useEffect(() => {
-  //   getFiles(setOwnedFiles, setRecentFiles, setStarredFiles);
-  // }, []);
+  const fileId = history.location.state.fileId;
+  const fileUrl = history.location.state.url;
+  const fileName = history.location.state.name;
 
   return (
     <>
       <Header />
       <Container className="mt--7 pb-7" fluid>
-        <FilePreviewer
-          file={{
-            url: url,
-          }}
-        />
+        <Row>
+          <Col className="mb-5 mb-xl-0" xl="8">
+            <h3 className="text-secondary">{fileName}</h3>
+          </Col>
+        </Row>
+        <ImageViewer fileUrl={fileUrl} fileId={fileId} />
       </Container>
     </>
   );
