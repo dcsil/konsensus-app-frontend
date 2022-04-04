8fd360e5bafa2badc4dd172b62e192b3ce330b1b
Safa <safa.alsiaudi@gmail.com>
2022-04-04 19:10:36 -0400

    works with this version

--- client/src/views/Shared.js
+++ client/src/views/Shared.js
@@ -33,8 +33,12 @@ import FileRow from 'components/Files/FileRow.js';
 
 
 const Shared = () => {
-  const dummyfiledata = [{filename: 'file1', filesize: 800 }, {filename: 'file2', filesize: 700 }, {filename: 'file3', filesize: 900 }]; 
-
+  const dummyfiledata = [
+    {filename: 'FileName.pdf', filesize: '123.45 Gb', status: 'Warning', collaborators: ['Ryan Tompson', 'Romina Hadid', 'Alexander Smith', 'Jessica Doe'], progress: '60%', timestamp: 'Apr 01 2022 06:42:57'}, 
+    {filename: 'file2.docx', filesize: '43.45 Gb', status: 'Completed', collaborators: ['Felisha Keys','Draco Rico'], progress: '100%', timestamp: 'Apr 01 2022 04:10:41'}, 
+    {filename: 'file3.ppt', filesize: '2.45 Gb', status: 'On Schedule', collaborators: ['Bella Hadid', 'Will Smith', 'Chris Rock'], progress: '80%', timestamp: 'Mar 27 2022 12:38:01'},
+    {filename: 'file4.xls', filesize: '3.45 Gb', status: 'Completed', collaborators: ['Britney Spierres'], progress: '100%', timestamp: 'Mar 15 2022 14:25:13'},
+    {filename: 'file5.txt', filesize: '123.45 Mb', status: 'Completed', collaborators: ['Bob Flenderson'], progress: '100%', timestamp: 'Feb 04 2022 08:42:45'}] 
   return (
     <>
       <Header />
@@ -64,10 +68,9 @@ const Shared = () => {
                 </thead>
                 <tbody>
                 { dummyfiledata.map(filerow => (
-                  <FileRow>
+                  <FileRow fileInfo={ filerow }>
                   </FileRow>
-                ))} 
-
+                )) }
                 </tbody>
               </Table>
               <CardFooter className="py-4">
