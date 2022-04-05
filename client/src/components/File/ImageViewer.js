0a0e4d0af4203e16c9bddd192a5f31d234b0d02a
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-04 21:15:41 -0400

    reformatted image viewer component

--- client/src/components/File/ImageViewer.js
+++ client/src/components/File/ImageViewer.js
@@ -1,30 +1,59 @@
 import FilePreviewer from 'react-file-previewer';
 import React, { useEffect, useState } from 'react';
-import { Container, Col, Row } from 'reactstrap';
+import { Container, Col, Row, Button } from 'reactstrap';
 import OrgTable from 'components/Profile/OrgTable';
 import AccessHistoryTable from './AccessHistoryTable';
 import { getFileById } from 'components/Auth/authFunctions';
+import { getUserById } from 'components/Auth/authFunctions';
+import { useHistory } from 'react-router-dom';
 
-const ImageViewer = ({ fileUrl, fileId }) => {
-  const [actions, setActions] = useState([]);
+const ImageViewer = ({ actions, user }) => {
+  const history = useHistory();
+  const [tab, setTab] = useState('file');
+  const fileName = history.location.state.name;
+  const fileUrl = history.location.state.url;
 
-  useEffect(() => {
-    getFileById(fileId, setActions);
-  }, []);
   return (
-    <div style={{ alignItems: 'left' }}>
+    <div>
       <Row>
-        <Col>
-          <FilePreviewer
-            file={{
-              url: fileUrl,
-            }}
-          />
+        <Col className="mb-5 mb-xl-0" xl="8">
+          <h3 className="text-secondary">{fileName}</h3>
         </Col>
-        <Col>
-          <AccessHistoryTable actions={[actions]} />
-        </Col>
+        <Button
+          color="primary"
+          className={tab === 'history' ? null : 'active'}
+          size="sm"
+          type="button"
+          onClick={() => setTab('file')}
+        >
+          File View
+        </Button>
+        <Button
+          color="primary"
+          className={tab === 'file' ? null : 'active'}
+          size="sm"
+          type="button"
+          onClick={() => setTab('history')}
+        >
+          Access History
+        </Button>
       </Row>
+      <div style={{ alignItems: 'center', paddingTop: '4px' }}>
+        <Row>
+          <Col>
+            {tab === 'file' ? (
+              <FilePreviewer
+                style={{ width: '50px' }}
+                file={{
+                  url: fileUrl,
+                }}
+              />
+            ) : (
+              <AccessHistoryTable actions={[actions]} user={user} />
+            )}
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
