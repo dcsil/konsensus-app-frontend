5dce7e4b60c7e83069f83247c0e23dc9d0d0507f
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-04 21:15:15 -0400

    updated file page

--- client/src/views/File.js
+++ client/src/views/File.js
@@ -1,5 +1,5 @@
 import { getFiles } from 'components/Auth/authFunctions';
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 // reactstrap components
@@ -11,23 +11,29 @@ import { useState } from 'react';
 import Header from 'components/Headers/Header.js';
 import { useHistory } from 'react-router-dom';
 import ImageViewer from 'components/File/ImageViewer';
+import { getFileById } from 'components/Auth/authFunctions';
+import { getUserById } from 'components/Auth/authFunctions';
 
 const File = () => {
   let history = useHistory();
   const fileId = history.location.state.fileId;
   const fileUrl = history.location.state.url;
   const fileName = history.location.state.name;
+  const lastUpdater = history.location.state.lastUpdater;
+
+  const [actions, setActions] = useState([]);
+  const [user, setUser] = useState(null);
+  console.log(lastUpdater);
+  useEffect(() => {
+    getFileById(fileId, setActions);
+    getUserById(lastUpdater, setUser);
+  }, []);
 
   return (
     <>
       <Header />
       <Container className="mt--7 pb-7" fluid>
-        <Row>
-          <Col className="mb-5 mb-xl-0" xl="8">
-            <h3 className="text-secondary">{fileName}</h3>
-          </Col>
-        </Row>
-        <ImageViewer fileUrl={fileUrl} fileId={fileId} />
+        <ImageViewer actions={actions} user={user} />
       </Container>
     </>
   );
