20c0e42516d521175098cb71df34d61901099d00
Safa <safa.alsiaudi@gmail.com>
2022-04-06 14:29:17 -0400

    removed dummy data from starred and shared

--- client/src/views/Shared.js
+++ client/src/views/Shared.js
@@ -15,6 +15,10 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+
+import { GlobalStateContext } from 'components/GlobalState';
+import { getFiles } from 'components/Auth/authFunctions';
+
 // reactstrap components
 import {
   Card,
@@ -28,17 +32,23 @@ import {
   Row,
 } from 'reactstrap';
 // core components
+
+import { useEffect, useState, useContext } from 'react';
+
 import Header from 'components/Headers/Header.js';
 import FileRow from 'components/Files/FileRow.js';
 
 
-const Shared = () => {
-  const dummyfiledata = [
-    {filename: 'FileName.pdf', filesize: '123.45 Gb', status: 'Warning', collaborators: ['Ryan Tompson', 'Romina Hadid', 'Alexander Smith', 'Jessica Doe'], progress: '60%', timestamp: 'Apr 01 2022 06:42:57'}, 
-    {filename: 'file2.docx', filesize: '43.45 Gb', status: 'Completed', collaborators: ['Felisha Keys','Draco Rico'], progress: '100%', timestamp: 'Apr 01 2022 04:10:41'}, 
-    {filename: 'file3.ppt', filesize: '2.45 Gb', status: 'On Schedule', collaborators: ['Bella Hadid', 'Will Smith', 'Chris Rock'], progress: '80%', timestamp: 'Mar 27 2022 12:38:01'},
-    {filename: 'file4.xls', filesize: '3.45 Gb', status: 'Completed', collaborators: ['Britney Spierres'], progress: '100%', timestamp: 'Mar 15 2022 14:25:13'},
-    {filename: 'file5.txt', filesize: '123.45 Mb', status: 'Completed', collaborators: ['Bob Flenderson'], progress: '100%', timestamp: 'Feb 04 2022 08:42:45'}] 
+const Shared = (props) => {
+  const [ownedFiles, setOwnedFiles] = useState([]);
+
+  const globalState = useContext(GlobalStateContext);
+  useEffect(() => {
+    getFiles(
+      setOwnedFiles
+    );
+  }, []);
+
   return (
     <>
       <Header />
@@ -67,7 +77,7 @@ const Shared = () => {
                   </tr>
                 </thead>
                 <tbody>
-                { dummyfiledata.map(filerow => (
+                { ownedFiles.map(filerow => (
                   <FileRow fileInfo={ filerow }>
                   </FileRow>
                 )) }
--- client/src/views/Starred.js
+++ client/src/views/Starred.js
@@ -23,13 +23,6 @@ import FileRow from 'components/Files/FileRow.js';
 const Starred = (props) => {
   const [starredFiles, setStarredFiles] = useState([]);
   
-  const dummyfiledata = [
-    {filename: 'FileName.pdf', filesize: '123.45 Gb', status: 'Warning', collaborators: ['Ryan Tompson', 'Romina Hadid', 'Alexander Smith', 'Jessica Doe'], progress: '60%', timestamp: 'Apr 01 2022 06:42:57'}, 
-    {filename: 'file2.docx', filesize: '43.45 Gb', status: 'Completed', collaborators: ['Felisha Keys','Draco Rico'], progress: '100%', timestamp: 'Apr 01 2022 04:10:41'}, 
-    {filename: 'file3.ppt', filesize: '2.45 Gb', status: 'On Schedule', collaborators: ['Bella Hadid', 'Will Smith', 'Chris Rock'], progress: '80%', timestamp: 'Mar 27 2022 12:38:01'},
-    {filename: 'file4.xls', filesize: '3.45 Gb', status: 'Completed', collaborators: ['Britney Spierres'], progress: '100%', timestamp: 'Mar 15 2022 14:25:13'},
-    {filename: 'file5.txt', filesize: '123.45 Mb', status: 'Completed', collaborators: ['Bob Flenderson'], progress: '100%', timestamp: 'Feb 04 2022 08:42:45'}] 
-
   const globalState = useContext(GlobalStateContext);
   useEffect(() => {
     getFiles(
@@ -66,9 +59,7 @@ const Starred = (props) => {
                   <tr>
                     <th scope="col">Name</th>
                     <th scope="col">Size</th>
-                    <th scope="col">Status</th>
                     <th scope="col">Collaborators</th>
-                    <th scope="col">Completion</th>
                     <th scope="col">Last Access</th>
                     <th scope="col" />
                   </tr>
