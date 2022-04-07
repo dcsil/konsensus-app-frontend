df7aa1cb86557baf5b5433800f08e06d59fd6f44
Stella Cai <stella.sijiacai@gmail.com>
2022-04-07 02:35:21 -0400

    collaborators in filerow

--- client/src/components/File/FileRow.js
+++ client/src/components/File/FileRow.js
@@ -6,12 +6,13 @@ import {
   Media,
   UncontrolledTooltip,
 } from 'reactstrap';
-import React from "react";
+import React, { createRef } from "react";
 import Moment from 'react-moment';
 import { useHistory } from 'react-router-dom';
+import Avatar from 'components/Profile/Avatar';
 
 const FileRow = (props) => {
-  const { id, name, size, updatedAt, lastUpdater, type } = props.fileInfo;
+  const { id, name, size, updatedAt, lastUpdater, type, collaborators } = props.file;
   const history = useHistory();
 
   const handleClick = () => {
@@ -28,7 +29,7 @@ const FileRow = (props) => {
   };
 
   return (
-    <tr onClick={handleClick} style={{cursor:'pointer'}}>
+    <tr onClick={handleClick} style={{ cursor: 'pointer' }}>
       <th scope="row">
         <Media className="align-items-center">
           <i className="ni ni-single-copy-04 text-primary pr-3" />
@@ -47,93 +48,7 @@ const FileRow = (props) => {
         </span>
       </td>
       <td>
-        <div className="avatar-group">
-          <a
-            className="avatar avatar-sm"
-            href="#pablo"
-            id="tooltip742438047"
-            onClick={(e) => e.preventDefault()}
-          >
-            <img
-              alt="..."
-              className="rounded-circle"
-              src={
-                require('../../assets/img/theme/team-1-800x800.jpg')
-                  .default
-              }
-            />
-          </a>
-          <UncontrolledTooltip
-            delay={0}
-            target="tooltip742438047"
-          >
-            Ryan Tompson
-            {/* {collaborators[1]} */}
-          </UncontrolledTooltip>
-          <a
-            className="avatar avatar-sm"
-            href="#pablo"
-            id="tooltip941738690"
-            onClick={(e) => e.preventDefault()}
-          >
-            <img
-              alt="..."
-              className="rounded-circle"
-              src={
-                require('../../assets/img/theme/team-2-800x800.jpg')
-                  .default
-              }
-            />
-          </a>
-          <UncontrolledTooltip
-            delay={0}
-            target="tooltip941738690"
-          >
-            Romina Hadid
-          </UncontrolledTooltip>
-          <a
-            className="avatar avatar-sm"
-            href="#pablo"
-            id="tooltip804044742"
-            onClick={(e) => e.preventDefault()}
-          >
-            <img
-              alt="..."
-              className="rounded-circle"
-              src={
-                require('../../assets/img/theme/team-3-800x800.jpg')
-                  .default
-              }
-            />
-          </a>
-          <UncontrolledTooltip
-            delay={0}
-            target="tooltip804044742"
-          >
-            Alexander Smith
-          </UncontrolledTooltip>
-          <a
-            className="avatar avatar-sm"
-            href="#pablo"
-            id="tooltip996637554"
-            onClick={(e) => e.preventDefault()}
-          >
-            <img
-              alt="..."
-              className="rounded-circle"
-              src={
-                require('../../assets/img/theme/team-4-800x800.jpg')
-                  .default
-              }
-            />
-          </a>
-          <UncontrolledTooltip
-            delay={0}
-            target="tooltip996637554"
-          >
-            Jessica Doe
-          </UncontrolledTooltip>
-        </div>
+        <Collaborators collaborators={collaborators} />
       </td>
       <td> <Moment
         format="MMM DD/YYYY hh:mm"
@@ -179,8 +94,39 @@ const FileRow = (props) => {
     </tr>
   )
 }
+
+const Collaborators = ({ collaborators }) => {
+
+  const renderAvatar = (collaborator) => {
+    const ref = createRef();
+
+    return (
+      <React.Fragment key={collaborator.id}>
+        <a
+          className="avatar avatar-sm"
+          href={`${collaborator.firstName} ${collaborator.lastName}`}
+          ref={ref}
+          id={collaborator.id}
+          onClick={(e) => e.preventDefault()}
+        >
+          <Avatar name={`${collaborator.firstName} ${collaborator.lastName}`} url={collaborator.image} />
+        </a>
+
+        <UncontrolledTooltip
+          delay={0}
+          target={ref}
+        >
+          {`${collaborator.firstName} ${collaborator.lastName}`}
+        </UncontrolledTooltip>
+      </React.Fragment>
+    )
+  }
+
+  return (
+    <div className="avatar-group">
+      {collaborators.map(collaborator => renderAvatar(collaborator))}
+    </div>
+  )
+}
+
 export default FileRow;
-
-
-
-
