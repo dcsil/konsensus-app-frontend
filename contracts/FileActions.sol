38d42bdea82b0bdaaf9a2b4daffbf485f7995144
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 14:42:42 -0400

    updated file contract

--- contracts/FileActions.sol
+++ contracts/FileActions.sol
@@ -2,22 +2,23 @@ pragma solidity >=0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract FileActions {
-    mapping(uint256 => FileAction[]) public fileMappings; // state variable
+    mapping(string => FileAction[]) public fileMappings; // state variable
     uint256 public fileMappingCount;
 
     struct FileAction {
         // uint id;
-        uint256 fileId; // File Id should match Id in Konsensus Backend
+        string fileId; // File Id should match Id in Konsensus Backend
         string fileName;
         string date;
-        uint256 userId; // User attributes
+        string userId; // User attributes
+        string userName;
         string action;
         string role;
     }
 
     constructor() public {}
 
-    function getAllFileActionsForFile(uint256 _fileId)
+    function getAllFileActionsForFile(string memory _fileId)
         public
         view
         returns (FileAction[] memory)
@@ -27,16 +28,25 @@ contract FileActions {
 
     // Adds a new file action with fileId <id> and
     function createFileAction(
-        uint256 _fileId,
+        string memory _fileId,
         string memory _fileName,
         string memory _date,
-        uint256 _userId,
+        string memory _userId,
+        string memory _userName,
         string memory _action,
         string memory _role
     ) public {
         fileMappingCount++;
         fileMappings[_fileId].push(
-            FileAction(_fileId, _fileName, _date, _userId, _action, _role)
+            FileAction(
+                _fileId,
+                _fileName,
+                _date,
+                _userId,
+                _userName,
+                _action,
+                _role
+            )
         );
     }
 }
