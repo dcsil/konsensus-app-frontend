54d7b4e74d1bdd4351e9b1e52a4755ddd46766d7
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 14:43:13 -0400

    updated global state values

--- client/src/components/GlobalState.js
+++ client/src/components/GlobalState.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 // Set up global contexts
 export const GlobalStateContext = React.createContext();
 export const GlobalDispatchContext = React.createContext();
 // Actions
 export const SET_TOKEN = 'SET_TOKEN';
+export const SET_USER = 'SET_USER';
+
 // Reducer
 export const reducer = (state, action) => {
   const { type, payload } = action;
@@ -16,6 +18,19 @@ export const reducer = (state, action) => {
       return {
         ...state,
         token: payload,
+      };
+    }
+    case SET_USER: {
+      Cookies.set('userId', payload.id);
+      Cookies.set(
+        'userName',
+        `${payload.firstName} ${payload.lastName}`
+      );
+      Cookies.set('userRole', payload.role);
+
+      return {
+        ...state,
+        user: payload,
       };
     }
     // Add more here!
