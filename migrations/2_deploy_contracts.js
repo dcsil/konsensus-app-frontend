9d1a293c6d82f434b1707430e24ad02e6a3830fd
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 13:20:16 -0400

    added contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
-const HelloWorld = artifacts.require("HelloWorld");
-const SimpleStorage = artifacts.require("SimpleStorage");
+const HelloWorld = artifacts.require('HelloWorld');
+const SimpleStorage = artifacts.require('SimpleStorage');
+const FileActions = artifacts.require('FileActions');
+const Contacts = artifacts.require('Contacts');
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(HelloWorld);
   deployer.deploy(SimpleStorage);
+  deployer.deploy(FileActions);
+  deployer.deploy(Contacts);
 };
