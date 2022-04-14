5921332cbc1802d50a797889d339bf7273ec138f
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 22:45:12 -0400

    admin footer test

--- client/src/components/Footers/AdminFooter.js
+++ client/src/components/Footers/AdminFooter.js
@@ -1,24 +1,7 @@
-/*!
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 
-=========================================================
-* Argon Dashboard React - v1.2.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
-
-// reactstrap components
-import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
+import { Row, Col } from 'reactstrap';
 
 const Footer = () => {
   return (
@@ -26,7 +9,7 @@ const Footer = () => {
       <Row className="align-items-center justify-content-xl-between">
         <Col xl="6">
           <div className="copyright text-center text-xl-left text-muted">
-            © {new Date().getFullYear()}{" "}
+            © {new Date().getFullYear()}{' '}
             <a
               className="font-weight-bold ml-1"
               rel="noopener noreferrer"
