703a2bb48a0f5a1e2705437318928a4753a7fb14
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 22:52:55 -0400

    file row test

---
+++ client/src/components/FileRow.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FileRow from './File/FileRow';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+test('Test Sidebar component', () => {
+  const wrapper = shallow(<FileRow />);
+  expect(wrapper).toMatchSnapshot();
+});
