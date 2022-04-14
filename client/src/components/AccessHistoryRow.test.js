8e0b9cc7058e297d9568466fe0c9c70b5c6ca43d
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 23:02:41 -0400

    access history row test

---
+++ client/src/components/AccessHistoryRow.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AccessHistoryRow from './File/AccessHistoryRow';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+test('Test Sidebar component', () => {
+  const wrapper = shallow(<AccessHistoryRow />);
+  expect(wrapper).toMatchSnapshot();
+});
