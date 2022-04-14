5edaa2a478b70b78709c1eedef58bea9d45aa939
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 22:42:17 -0400

    upload modal test

---
+++ client/src/components/UploadModal.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import UploadModal from './Modals/UploadModal';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+test('Test Sidebar component', () => {
+  const wrapper = shallow(<UploadModal />);
+  expect(wrapper).toMatchSnapshot();
+});
