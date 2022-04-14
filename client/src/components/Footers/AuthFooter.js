762c6dc6c0dbf8f92c8acc5ace857b55d86f9cce
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 22:45:57 -0400

    auth footer test

--- client/src/components/Footers/AuthFooter.js
+++ client/src/components/Footers/AuthFooter.js
@@ -1,24 +1,16 @@
-/*!
+import React from 'react';
 
-=========================================================
-* Argon Dashboard React - v1.2.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 /*eslint-disable*/
 
 // reactstrap components
-import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
+import {
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
 
 const Login = () => {
   return (
@@ -28,11 +20,8 @@ const Login = () => {
           <Row className="align-items-center justify-content-xl-between">
             <Col xl="6">
               <div className="copyright text-center text-xl-left text-muted">
-                © {new Date().getFullYear()}{" "}
-                <a
-                  className="font-weight-bold ml-1"
-                  target="_blank"
-                >
+                © {new Date().getFullYear()}{' '}
+                <a className="font-weight-bold ml-1" target="_blank">
                   Konsensus
                 </a>
               </div>
