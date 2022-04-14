e6094fd0e82a86d805514bc8b888f2bd6063ccd0
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-13 22:32:59 -0400

    admin navbar

---
+++ client/src/components/AdminNavbar.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AdminNavbar from './Navbars/AdminNavbar';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+test('Test Sidebar component', () => {
+  const wrapper = shallow(<AdminNavbar />);
+  expect(wrapper).toMatchSnapshot();
+});
