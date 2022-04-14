f16360a8c4744261c9795c112fe6e72c6b51a2ae
Windosaurus <42529415+Windosaurus@users.noreply.github.com>
2022-04-14 00:00:19 -0400

    circle ci fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
 const HelloWorld = artifacts.require('HelloWorld');
 const SimpleStorage = artifacts.require('SimpleStorage');
 const FileActions = artifacts.require('FileActions');
-const Contacts = artifacts.require('Contacts');
 
 module.exports = function (deployer) {
   deployer.deploy(HelloWorld);
   deployer.deploy(SimpleStorage);
   deployer.deploy(FileActions);
-  deployer.deploy(Contacts);
 };
